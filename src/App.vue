19c9464f7ba06543ad18177007ef2d04071a66fb
koljapluemer <github@koljapluemer.com>
2025-05-26 10:53:22 +0200

    setup vue and local link learn components from igu

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+<igu-flashcard>
+  <span slot="front-title">Sierra Leone</span>
+      <span slot="front-content">Capital?</span>
+      <span slot="back-title">Freetown</span>
+      <span slot="back-content"></span>
+</igu-flashcard>
+</template>
